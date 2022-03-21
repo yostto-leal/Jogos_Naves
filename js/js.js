45da9996ea4180e7a241cd002adbe6551bf53446
ary Leal <yosttoleal@gmail.com>
2022-03-21 01:53:45 -0300

    fim cilisao

--- js/js.js
+++ js/js.js
@@ -226,6 +226,20 @@ function start() { // Inicio da funcão start()
         reposicionaAmigo();
         $("#amigo").remove();
         }
+
+        //Inimigo2 com o amigo
+		
+if (colisao6.length>0) {
+	    
+    amigoX = parseInt($("#amigo").css("left"));
+    amigoY = parseInt($("#amigo").css("top"));
+    explosao3(amigoX,amigoY);
+    $("#amigo").remove();
+            
+    reposicionaAmigo();
+            
+    }
+    
             
             } //Fim da função colisao()
 
@@ -292,6 +306,23 @@ function explosao1(inimigo1X,inimigo1Y) {
             
             
         } // Fim da função explosao2()
+
+        //Explosão3
+	
+function explosao3(amigoX,amigoY) {
+    $("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+    $("#explosao3").css("top",amigoY);
+    $("#explosao3").css("left",amigoX);
+    var tempoExplosao3=window.setInterval(resetaExplosao3, 1000);
+    function resetaExplosao3() {
+    $("#explosao3").remove();
+    window.clearInterval(tempoExplosao3);
+    tempoExplosao3=null;
+            
+    }
+    
+    } // Fim da função explosao3
+    
     
             
         
