1eea2a853dc9532974ba0354578f5dce40902133
ary Leal <yosttoleal@gmail.com>
2022-03-21 01:43:28 -0300

    disparos e colisoões

--- js/js.js
+++ js/js.js
@@ -10,6 +10,7 @@ function start() { // Inicio da funcão start()
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
     //Principais vari�veis do jogo
+    var fimdejogo=false;
     var jogo = {}
     var velocidade=5;
     var posicaoY = parseInt(Math.random() * 334);
@@ -170,12 +171,129 @@ function start() { // Inicio da funcão start()
 
         function colisao() {
             var colisao1 = ($("#jogador").collision($("#inimigo1")));
+            var colisao2 = ($("#jogador").collision($("#inimigo2")));
+            var colisao3 = ($("#disparo").collision($("#inimigo1")));
+            var colisao4 = ($("#disparo").collision($("#inimigo2")));
+            var colisao5 = ($("#jogador").collision($("#amigo")));
+            var colisao6 = ($("#inimigo2").collision($("#amigo")));
+
+
+
             // jogador com o inimigo1
+                
+                if (colisao1.length>0) {
+                    
+                inimigo1X = parseInt($("#inimigo1").css("left"));
+                inimigo1Y = parseInt($("#inimigo1").css("top"));
+                explosao1(inimigo1X,inimigo1Y);
+            
+                posicaoY = parseInt(Math.random() * 334);
+                $("#inimigo1").css("left",694);
+                $("#inimigo1").css("top",posicaoY);
+                }
+
+                if (colisao2.length>0) {
+	
+                    inimigo2X = parseInt($("#inimigo2").css("left"));
+                    inimigo2Y = parseInt($("#inimigo2").css("top"));
+                    explosao2(inimigo2X,inimigo2Y);
+                            
+                    $("#inimigo2").remove();
+                        
+                    reposicionaInimigo2();
+                        
+                    }	
+
+                    // Disparo com o inimigo2
+		
+	if (colisao4.length>0) {
+		
+        inimigo2X = parseInt($("#inimigo2").css("left"));
+        inimigo2Y = parseInt($("#inimigo2").css("top"));
+        $("#inimigo2").remove();
+    
+        explosao2(inimigo2X,inimigo2Y);
+        $("#disparo").css("left",950);
         
-            console.log(colisao1);
+        reposicionaInimigo2();
+            
+        }
+
+        // jogador com o amigo
+		
+	if (colisao5.length>0) {
+		
+        reposicionaAmigo();
+        $("#amigo").remove();
+        }
+            
+            } //Fim da função colisao()
+
+            //Explosão 1
+function explosao1(inimigo1X,inimigo1Y) {
+	$("#fundoGame").append("<div id='explosao1'></div");
+	$("#explosao1").css("background-image", "url(imgs/explosao.png)");
+	var div=$("#explosao1");
+	div.css("top", inimigo1Y);
+	div.css("left", inimigo1X);
+	div.animate({width:200, opacity:0}, "slow");
+	
+	var tempoExplosao=window.setInterval(removeExplosao, 1000);
+	
+		function removeExplosao() {
+			
+			div.remove();
+			window.clearInterval(tempoExplosao);
+			tempoExplosao=null;
+			
+		}
+		
+	} // Fim da função explosao1()
+
+    //Reposiciona Inimigo2
+	
+	function reposicionaInimigo2() {
+	
+        var tempoColisao4=window.setInterval(reposiciona4, 5000);
+            
+            function reposiciona4() {
+            window.clearInterval(tempoColisao4);
+            tempoColisao4=null;
+                
+                if (fimdejogo==false) {
+                
+                $("#fundoGame").append("<div id=inimigo2></div");
+                
+                }
+                
+            }	
+        }	
+
+        //Explosão2
+	
+	function explosao2(inimigo2X,inimigo2Y) {
+	
+        $("#fundoGame").append("<div id='explosao2'></div");
+        $("#explosao2").css("background-image", "url(imgs/explosao.png)");
+        var div2=$("#explosao2");
+        div2.css("top", inimigo2Y);
+        div2.css("left", inimigo2X);
+        div2.animate({width:200, opacity:0}, "slow");
         
-        } //Fim da função colisao()
+        var tempoExplosao2=window.setInterval(removeExplosao2, 1000);
         
+            function removeExplosao2() {
+                
+                div2.remove();
+                window.clearInterval(tempoExplosao2);
+                tempoExplosao2=null;
+                
+            }
+            
+            
+        } // Fim da função explosao2()
+    
+            
         
         
     
